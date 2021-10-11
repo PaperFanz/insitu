350a8075b2bec65d161a495f2e92a0a23e6ed9ab
Pete Fan <petefan.7@gmail.com>
2021-10-11 10:37:25 -0500

    update format script

--- format.sh
+++ format.sh
@@ -1,1 +1,33 @@
-find . -name '*.hpp' -or -name '*.cpp' | xargs clang-format -i -style=file $1
+#!/bin/bash
+
+set -e
+
+function append_newline {
+    if [[ -z "$(tail -c 1 "$1")" ]]; then
+        echo "$1"
+    else
+        echo >> "$1"
+    fi
+}
+
+if [ -z "$1" ]; then
+    TARGET_DIR="."
+else
+    TARGET_DIR=$1
+fi
+
+pushd ${TARGET_DIR} >> /dev/null
+
+# Find all source files using Git to automatically respect .gitignore
+FILES=$(git ls-files "*.hpp" "*.cpp")
+
+# Run clang-format
+clang-format -i -style=file ${FILES}
+
+# Check newlines
+for f in ${FILES}; do
+    append_newline $f
+done
+
+popd >> /dev/null
+
