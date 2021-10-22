5a3c01301b24f905524627a332d4e0b1bda4cb07
Pete Fan <petefan.7@gmail.com>
2021-10-22 10:56:52 -0500

    add ros topic util function to insitu_utils

---
+++ insitu_utils/src/ros.cpp
@@ -0,0 +1,21 @@
+#include "insitu_utils/ros.hpp"
+#include <ros/master.h>
+
+namespace insitu_utils
+{
+    
+void getTopicsByType(std::vector<std::string> &ret, const std::string& type)
+{
+    ros::master::V_TopicInfo topic_infos;
+    ros::master::getTopics(topic_infos);
+
+    ret.clear();
+    for (const auto& topic : topic_infos) {
+        if (topic.datatype == type || type == "") {
+            ret.push_back(topic.name);
+        }
+    }
+}
+
+}
+
