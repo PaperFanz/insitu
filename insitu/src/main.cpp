2a21501d88da6b5c4333564be8a7c76d740a483f
Pete Fan <petefan.7@gmail.com>
2021-10-25 14:31:44 -0500

    Ctrl+C now properly exits insitu

--- insitu/src/main.cpp
+++ insitu/src/main.cpp
@@ -13,11 +13,21 @@ int main(int argc, char** argv)
     /* GUI INITIALIZATION */
     QApplication app(argc, argv);
 
+    std::thread kill_thread([&app] {
+        ros::Rate loop_rate(4);
+        while(ros::ok())
+        {
+            loop_rate.sleep();
+        }
+        app.exit();
+     });
+
     insitu::MainWindow w(argc, argv);
     w.show();
 
     app.connect(&app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()));
     int result = app.exec();
+    if(kill_thread.joinable()) kill_thread.join();
 
     return result;
 }
